70eb95491dc0a10ecdb20ebf5101573e7cfb75fc
Talgat Muratbekov <talgat2307@gmail.com>
2020-09-05 19:26:44 +0600

    #1 - Prepared all files for the project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Route, Switch} from 'react-router-dom';
+import Layout from './components/Layout/Layout';
 import './App.css';
+import Home from './containers/Home/Home';
+import About from './containers/Pages/About/About';
+import Contacts from './containers/Pages/Contacts/Contacts';
+import Divisions from './containers/Pages/Divisions/Divisions';
+import Admin from './containers/Admin/Admin';
+import Partners from './containers/Pages/Partners/Partners';
+import Settings from './containers/Pages/Settings/Settings';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Layout>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/pages/about' component={About}/>
+        <Route path='/pages/contacts' component={Contacts}/>
+        <Route path='/pages/divisions' component={Divisions}/>
+        <Route path='/pages/partners' component={Partners} />
+        <Route path='/pages/settings' component={Settings} />
+        <Route path='/pages/admin' component={Admin} />
+      </Switch>
+    </Layout>
+  </div>
+);
 
 export default App;
